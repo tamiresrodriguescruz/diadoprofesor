b7793c7e6307ebfbf42cb18456677de50e93a937
tamiresrodriguescruz <138024756+tamiresrodriguescruz@users.noreply.github.com>
2023-10-11 11:18:09 -0300

    tamires

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     mostrarMensagemBtn.addEventListener("click", function () {
         mensagem.innerHTML = "Queridos professores, hoje é o dia de celebrar vocês, " +
-        "os verdadeiros heróis que moldam mentes e inspiram sonhos. " +
+        "Ser professor é ser um condutor de almas e sonhos é lapidar mentes para que alcamcem o seu melhor . " +
         "Em cada sala de aula, vocês desempenham um papel vital na formação do futuro. " +
-        "Seu comprometimento, paixão e dedicação são admiráveis e dignos de reconhecimento. " +
+        "Aprender a ensinar para viver para educar . " +
         "Vocês não apenas compartilham conhecimento, mas também cultivam valores, orientam e apoiam os sonhos de seus alunos. " +
         "Com paciência infinita, vocês transformam desafios em oportunidades de aprendizado e capacitam os jovens a serem cidadãos responsáveis e pensadores críticos. " +
         "Neste Dia dos Professores, quero expressar minha mais profunda gratidão por tudo o que vocês fazem. " +
         "Vocês são faróis de luz na jornada educacional de todos nós, iluminando o caminho com sabedoria e compreensão. " +
-        "Obrigado por inspirarem, orientarem e ajudarem a construir um futuro melhor para todos. " +
+        "Que sua jornada seja repleta de coisas boas e realizações  . " +
         "Feliz Dia dos Professores!";
     });
 });
